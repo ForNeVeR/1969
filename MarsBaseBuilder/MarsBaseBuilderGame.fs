89d0ef54d4c0028d0dcd94520bdc58696289f200
Friedrich von Never <friedrich@fornever.me>
2017-05-24 23:43:10 +0700

    #4: install MonoGame

---
+++ MarsBaseBuilder/MarsBaseBuilderGame.fs
@@ -0,0 +1,12 @@
+﻿namespace MarsBaseBuilder
+
+open Microsoft.Xna.Framework
+
+type MarsBaseBuilderGame() =
+    inherit Game()
+
+    let marsColor = Color.IndianRed
+
+    override this.Draw(gameTime) =
+        this.GraphicsDevice.Clear(marsColor)
+        base.Draw(gameTime)
--- MarsBaseBuilder/Program.fs
+++ MarsBaseBuilder/Program.fs
@@ -1,3 +1,14 @@
-﻿[<EntryPoint>]
-let main argv = 
+﻿module MarsBaseBuilder.Program
+
+open Microsoft.Xna.Framework
+
+[<EntryPoint>]
+let main argv =
+    printfn "Starting game"
+    use game = new MarsBaseBuilderGame()
+    use graphics = new GraphicsDeviceManager(game)
+
+    game.Run()
+
+    printfn "Exiting game"
     0
