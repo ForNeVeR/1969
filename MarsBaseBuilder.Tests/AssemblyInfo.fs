9d31fc19b606852c9642cdfca380f39d40d8113d
Friedrich von Never <friedrich@fornever.me>
2017-05-28 23:48:32 +0700

    #6: add unit tests for the new logic

---
+++ MarsBaseBuilder.Tests/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace MarsBaseBuilder.Tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("MarsBaseBuilder.Tests")>]
+[<assembly: AssemblyProduct("MarsBaseBuilder")>]
+[<assembly: AssemblyCopyright("Copyright © MarsBaseBuilder contributors 2017")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("63d73c3c-918a-4917-a4d8-822bcf96fb7d")>]
+[<assembly: AssemblyVersion("0.0.1.0")>]
+
+()
---
+++ MarsBaseBuilder.Tests/GameLogicTests.fs
@@ -0,0 +1,23 @@
+﻿module MarsBaseBuilder.Tests.GameLogicTests
+
+open System
+
+open Microsoft.Xna.Framework
+open Xunit
+
+open MarsBaseBuilder
+
+let minute = TimeSpan.FromMinutes(1.0)
+let gameMinute = GameTime(minute, minute)
+
+[<Fact>]
+let ``New mission should include a Base`` () =
+    let state = GameLogic.newMission
+    Assert.Contains(Base, state.units)
+
+[<Fact>]
+let ``Update should be callable`` () =
+    let state = GameLogic.newMission
+    let state' = GameLogic.update state gameMinute
+    Assert.Equal(state, state')
+    
